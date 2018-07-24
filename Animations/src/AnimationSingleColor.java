77eb5f289c4a5a2aeb92c6547963a09e9edbda29
Amir Blum <amirgiraffe@gmail.com>
2018-07-24 03:22:59 +0300

    add breath animation

--- Animations/src/AnimationSingleColor.java
+++ Animations/src/AnimationSingleColor.java
@@ -1,7 +1,7 @@
 public class AnimationSingleColor extends AnimationIfc {
 
     @Override
-    void setHSBColor(HSBColor c, int globalI, int controllerI, int stripI, int PixelI, double timePercent) {
+    void setHSBColor(HSBColor c, int globalI, int controllerI, int stripI, int PixelI, double timePercent, double absTimeSeconds) {
         c.hue = timePercent;
         c.saturation = 1.0;
         c.brightness = 1.0;
